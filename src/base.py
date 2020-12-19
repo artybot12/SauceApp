c91a49a3b5df16c7aff650bd386241e2041b9a37
ArshdeepGadhri <arshdeep.gadhri@mail.utoronto.ca>
2020-12-19 15:09:51 -0500

    Added functionality, webapp is now useable

--- src/base.py
+++ src/base.py
@@ -5,6 +5,7 @@ import shutil
 import uvicorn
 import tracemoepy
 import asyncio
+import requests
 
 app = FastAPI()
 templates = Jinja2Templates(directory="src/templates")
@@ -16,20 +17,47 @@ def home(request: Request):
 
 
 @app.post("/sauce")
-def sauce(image: UploadFile = File(...)):
-    
+def sauce(image: UploadFile = File(...)): 
     temp_file = _save_file_to_disk(image, path="temp", save_as="temp")
-    data = get_data(temp_file)
-    print(data["RawDocsCount"])
-    return {"title": str(data["RawDocsCount"]), "all": data}
+    if not temp_file:
+        return
+    data = get_data(temp_file)['docs'][0]
+
+    return {"title": data["title_english"],
+            "title2": data["title_romaji"],
+            "season": data["season"],
+            "episode": data["episode"],
+            "time": getTime(data['at']),
+            "sim": getSim(str(data["similarity"])),
+            "preview": getPreview(data)
+            }
+
+def getPreview(data: dict):
+    uriEncoded = requests.utils.quote(data['filename'])
+    preview = "https://trace.moe/thumbnail.php?anilist_id=" + str(
+                data['anilist_id']) + "&file=" + uriEncoded.replace(" ", "") + "&t=" + str(
+                    data['at']) + "&token=" + str(data['tokenthumb'])
+    return preview
+
+def getTime(at: str):
+    p1, p2 = float(at) % 60,  float(at) / 60
+    p3, p2 = p2 % 60, p2 / 60
+    return str(int(p2)) + ":" + str(int(p3)) + ":" + str(int(p1))
+
+
+def getSim(sim: str):
+    return float(sim[0:4]) * 100
 
 
 def _save_file_to_disk(uploaded_file, path=".", save_as="default"):
     extension = os.path.splitext(uploaded_file.filename)[-1]
-    temp_file = os.path.join(path, save_as + extension)
-    with open(temp_file, "wb") as buffer:
-        shutil.copyfileobj(uploaded_file.file, buffer)
-    return temp_file
+    if extension in {"png", "jpeg"}:
+        temp_file = os.path.join(path, save_as + extension)
+        with open(temp_file, "wb") as buffer:
+            shutil.copyfileobj(uploaded_file.file, buffer)
+        return temp_file
+    return 
+
 
 def get_data(image):
     return tracemoe.search(image, encode=True)
